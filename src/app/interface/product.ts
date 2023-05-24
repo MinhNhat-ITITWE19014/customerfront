7739107697a8e131e8099207ef111c859b12906c
Nguyen Vo <dashzero123@gmail.com>
2023-05-24 17:34:18 +0700

    added product I/F and service for API calling

---
+++ src/app/interface/product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+  productID: string;
+  productName: string;
+  productDesc: string;
+  price: number;
+  productImgURL: string;
+  //category: Category;
+  //inventories: Inventory[];
+  //shoppingCarts: ShoppingCart[];
+}
---
+++ src/app/service/product.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../interface/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private baseUrl = 'http://localhost:8080/product';
+
+  constructor(private http: HttpClient) { }
+
+  createProduct(product: Product): Observable<any> {
+    return this.http.post(`${this.baseUrl}/create`, product);
+  }
+
+  getProducts(limit: number): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/list?limit=${limit}`);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/get/${id}`);
+  }
+
+  updateProduct(id: number, product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.baseUrl}/update/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseUrl}/delete/${id}`);
+  }
+}
