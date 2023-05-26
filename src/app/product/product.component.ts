fb416b28615ccc3b603547382f24268d00a5cc75
Nguyen Vo <dashzero123@gmail.com>
2023-05-26 23:28:59 +0700

    Product Information can now be access by ID.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../interface/product';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
-  selectedImage: string = 'assets/pimage1.png';
+export class ProductComponent implements OnInit {
+  product: Product | undefined;
 
-  changeImage(imageSrc: string) {
-    this.selectedImage = imageSrc;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const idProduct = this.activatedRoute.snapshot.params['id'];
+    this.fetchProduct(idProduct);
   }
+
+  fetchProduct(id: number): void {
+    this.productService.getProductById(id).subscribe(
+      (product: Product) => {
+        this.product = product;
+      },
+      (error) => {
+        console.error('Failed to fetch product:', error);
+      }
+    );
+  }
+
 
 }
