b8aefe8391cfe1af0a92b509191415da40f70c44
MinhNhat-ITITWE19014 <80942704+MinhNhat-ITITWE19014@users.noreply.github.com>
2023-05-27 12:24:34 +0700

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faAngleDown} from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,4 +12,5 @@ export class HeaderComponent {
   faUser = faUser;
   faCartShopping= faCartShopping;
   faAngleDown = faAngleDown;
+  faArrowRightFromBracket = faArrowRightFromBracket;
 }
